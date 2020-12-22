84a286056d7bc13f3a43c443eb9c39ea66f35a48
Back End <phpridder@gmail.com>
2020-12-23 05:06:22 +0600

    user registration api with error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function createUser(Request $request){
-        $validatedData = $request->validate([
+        $validator = Validator::make($request->all(), [
             'first_name' => ['required', 'max:255'],
             'last_name' => ['bail', 'required', 'max:255'],
         ]);
+
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
         $user = new User();
         $user->first_name = $request->first_name;
         $user->last_name = $request->last_name;
