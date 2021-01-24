cc4b97cb06405e2360da5d7fe2ebfbc0c0edabe0
Back End <phpridder@gmail.com>
2021-01-25 01:50:03 +0600

    test activity view data display

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -47,6 +47,7 @@ class ActivityController extends Controller
     }
     public function show(){
         $activities = Activity::all();
+        dd($activities);
         return view('activity',['activities',$activities]);
     }
 }
