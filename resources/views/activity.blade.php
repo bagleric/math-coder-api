063cb38c75522cc7a7b1af31c6e45840b431f07f
Back End <phpridder@gmail.com>
2021-01-25 02:47:55 +0600

    Activity show page created

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -1,29 +1,32 @@
-<table>
-    <tr>
-        <th>ID</th>
-        <th>User ID</th>
-        <th>Activity ID</th>
-        <th>Module ID</th>
-        <th>Started At</th>
-        <th>Ended At</th>
-        <th>No of Compiles</th>
-        <th>Completed</th>
-        <th>Compilation Timestamps</th>
-        <th>Screen Size</th>
-    </tr>
-
-    @foreach($activities as $activity)
+@if(count($activities))
+    <table>
         <tr>
-            <td>{{ $activity->id }}</td>
-            <td>{{ $activity->user_id }}</td>
-            <td>{{ $activity->activity_id }}</td>
-            <td>{{ $activity->module_id }}</td>
-            <td>{{ $activity->started_at }}</td>
-            <td>{{ $activity->ended_at }}</td>
-            <td>{{ $activity->no_of_compiles }}</td>
-            <td>{{ $activity->completed }}</td>
-            <td>{{ $activity->compilation_timestamps }}</td>
-            <td>{{ $activity->screen_size }}</td>
+            <th>ID</th>
+            <th>User ID</th>
+            <th>Activity ID</th>
+            <th>Module ID</th>
+            <th>Started At</th>
+            <th>Ended At</th>
+            <th>No of Compiles</th>
+            <th>Completed</th>
+            <th>Compilation Timestamps</th>
+            <th>Screen Size</th>
         </tr>
-    @endforeach
-</table>
+        @foreach($activities as $activity)
+            <tr>
+                <td>{{ $activity->id }}</td>
+                <td>{{ $activity->user_id }}</td>
+                <td>{{ $activity->activity_id }}</td>
+                <td>{{ $activity->module_id }}</td>
+                <td>{{ $activity->started_at }}</td>
+                <td>{{ $activity->ended_at }}</td>
+                <td>{{ $activity->no_of_compiles }}</td>
+                <td>{{ $activity->completed }}</td>
+                <td>{{ $activity->compilation_timestamps }}</td>
+                <td>{{ $activity->screen_size }}</td>
+            </tr>
+        @endforeach
+    </table>
+@else
+    No Data found
+@endif
