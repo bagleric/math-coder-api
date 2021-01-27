83732f8a5830f46d9a220be286ae622403a7736f
Back End <phpridder@gmail.com>
2021-01-27 16:45:40 +0600

    authenticable added for admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class Admin extends Model
+class Admin extends User
 {
-    use HasFactory, Notifiable;
+//    use HasFactory, Notifiable;
     protected $fillable = ['username', 'password'];
 }
