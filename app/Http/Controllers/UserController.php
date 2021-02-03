3dbda14834f7ffc44e0001cd9d6c387f23b061c4
Back End <phpridder@gmail.com>
2021-02-03 17:09:49 +0600

    user about debug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,22 +49,23 @@ class UserController extends Controller
         $id = $request->id;
         $user = User::find($id)->first();
 //        dd(collect($user));
-        $user = collect($user);
-        $user->answers = Answer::where('user_id',$id);
-//        $answers = Answer::where('user_id',$id);
+//        $user = collect($user);
+//        $user->answers = Answer::where('user_id',$id);
+        $answers = Answer::where('user_id',$id);
 //        if($answers){
 //            $user->answers = collect($answers);
 //        }
-        $user->event = Event::where('user_id',$id);
-//        $events = Event::where('user_id',$id);
+//        $user->event = Event::where('user_id',$id);
+        $events = Event::where('user_id',$id);
 //        if($events){
 //            $user->event = collect($events);
 //        }
-        $user->activities = Activity::where('user_id',$id);
-//        $activities = Activity::where('user_id',$id);
+//        $user->activities = Activity::where('user_id',$id);
+        $activities = Activity::where('user_id',$id);
 //        if($activities){
 //            $user->activities = collect($activities);
 //        }
+        dd($user,$activities,$events,$answers);
         return [
             'success'=>true,
             'about'=>$user,
